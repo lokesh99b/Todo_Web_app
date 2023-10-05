e599da05b30d9b6939be830e23da39b3a0ac5982
Lokesh <lokeshb031@gmail.com>
2023-10-05 19:13:55 +0530

    Todo Web App

---
+++ web.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import functions
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+todos = functions.get_todos()
+
+st.title("Todo List")
+st.subheader("")
+st.write("")
+
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="Enter a todo: ", placeholder="Add new Todo",
+              on_change=add_todo, key='new_todo')
